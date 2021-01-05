14764a803441557c746d34d464368a15bc6e39dc
Sadman Samee <sadman.tonmoy@gmail.com>
2021-01-05 22:27:40 +0600

    android project updated to latest

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_boilerplate/app/app.dart';
+import 'package:flutter_boilerplate/common/bloc/simple_bloc_delegate.dart';
+import 'package:flutter_boilerplate/common/constant/env.dart';
+
+void main() {
+  Bloc.observer = SimpleBlocDelegate();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runZonedGuarded(() {
+    runApp(App(env: EnvValue.staging));
+  }, (error, stackTrace) async {});
+}
